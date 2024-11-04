e5482a3c44c4b461b87a986f02b6c0825f39064f
tensei <tennsei716@gmail.com>
2024-11-04 09:01:32 +0800

    refactor(house_pkg/locate):对定位模块增加缓存功能、减少请求次数

--- miniprogram/house_pkg/pages/locate/index.js
+++ miniprogram/house_pkg/pages/locate/index.js
@@ -7,7 +7,9 @@ Page({
    */
   data: {
     address: '请定位',
-    points: []
+    points: [],
+    latitude: 0,
+    longitude: 0,
   },
 
   /**
@@ -21,24 +23,46 @@ Page({
    * 获取本地位置及附近小区
    */
   async getLocation() {
+    const app = getApp()
     const { latitude, longitude } = await wx.getLocation()
-    // 逆地址解析
-    await wx.qqMap.reverseGeocoder({
-      location: `${latitude},${longitude}`,
-      sig: `${wx.config.env.development.qqmap_SK}`,
-      success: (res) => {
-        this.setData({
-          address: res.result.address
-        })
-      },
-      fail: (res) => {
-        wx.showToast({
-          title: `定位失败:${res.message}`,
-          icon: 'none'
-        })
-      }
-    })
-    await this.getPoint(latitude, longitude)
+    // 缓存定位坐标、如果坐标相同则不再执行后面的逻辑、减少接口调用次数
+    if (typeof app.position === 'undefined') {
+      // 第一次定位
+      // 逆地址解析
+      await wx.qqMap.reverseGeocoder({
+        location: `${latitude},${longitude}`,
+        sig: `${wx.config.env.development.qqmap_SK}`,
+        success: (res) => {
+          this.setData({
+            address: res.result.address,
+            latitude,
+            longitude,
+          })
+        },
+        fail: (res) => {
+          wx.showToast({
+            title: `定位失败:${res.message}`,
+            icon: 'none'
+          })
+        }
+      })
+      await this.getPoint(latitude, longitude)
+    } else if (app.position.latitude === latitude && app.position.longitude === longitude) {
+      // 轻提示
+      wx.showToast({
+        title: '定位成功',
+        icon: 'none'
+      })
+      // 定位坐标相同、直接使用缓存数据
+      this.setData({
+        address: app.position.address,
+        latitude,
+        longitude,
+        points: app.position.points,
+      })
+      return true
+    }
+    return false
   },
 
   /**
@@ -57,6 +81,14 @@ Page({
         this.setData({
           points: res.data
         })
+        const app = getApp()
+        // 缓存定位信息
+        app.position = {
+          latitude,
+          longitude,
+          points: res.data,
+          address: this.data.address,
+        }
         console.log(res)
       },
       fail: (res) => {
